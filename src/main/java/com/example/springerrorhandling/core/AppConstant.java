b2a712dcde62b692678488d6fef84f921ffca22f
John Adeshola <timadeshola@gmail.com>
2021-12-01 16:44:26 +0100

    add appconstant

---
+++ src/main/java/com/example/springerrorhandling/core/AppConstant.java
@@ -0,0 +1,31 @@
+package com.example.springerrorhandling.core;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+
+import static java.time.format.DateTimeFormatter.ofPattern;
+
+/**
+ * Project title: spring-error-handling
+ *
+ * @author johnadeshola
+ * Date: 11/23/21
+ * Time: 6:04 PM
+ */
+public interface AppConstant {
+
+    public interface DateFormatters {
+        public SimpleDateFormat FORMATTER = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        public static final DateTimeFormatter DATE_TIME_FORMATTER = ofPattern("yyyy-MM-dd HH:mm");
+        public static final DateTimeFormatter DATE_TIME_LONG_FORMATTER = ofPattern("yyyy-MM-dd HH:mm:ss");
+        public static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        public static final String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS";
+        public static final String defaultDateTimeFormatter = "yyyy-MM-dd HH:mm:ss";
+        //        public static final Gson GSON = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").create();
+        public static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        public static final DateFormat dateFormat_ = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        public static final DateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        public static final DateFormat styleTime = new SimpleDateFormat("MMM dd, HH:mm a");
+    }
+}
